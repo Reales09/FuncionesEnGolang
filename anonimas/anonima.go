d59334bd7ce835442fb7dd5f0482a59eabfc2ccd
Reales Myles <rclarence123@gmail.com>
2023-04-28 00:23:33 -0500

    Closure

--- anonimas/anonima.go
+++ anonimas/anonima.go
@@ -2,20 +2,36 @@ package main
 
 import "fmt"
 
-func main() {
-	//Funcion anonima
-
-	/*
-		func() {
-			fmt.Println("Hola desde la funciona anonima")
-		}()
-	*/
-
-	myfunc := func(nombre string) string {
-		return fmt.Sprintf("Hola %s desde la funcion anonima", nombre)
-
+//Closure
+func repeat(n int) func(cadena string) string {
+	return func(cadena string) string {
+		var result string
+		for i := 0; i < n; i++ {
+			result += cadena
+		}
+		return result
 	}
 
-	fmt.Println(myfunc("Reales"))
+}
+
+func main() {
+
+	closure := repeat(5)
+	fmt.Println(closure("Hola"))
+	//Funcion anonima1
+
+	/*
+			func() {
+				fmt.Println("Hola desde la funciona anonima")
+			}()
+
+
+		myfunc := func(nombre string) string {
+			return fmt.Sprintf("Hola %s desde la funcion anonima", nombre)
+
+		}
+
+		fmt.Println(myfunc("Reales"))
+	*/
 
 }
